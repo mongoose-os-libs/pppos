mongoose-os-libs/pppos

commit 50ef31bc08927a19b80c97ca59f2b4812721430b

Fix flapping between disconnected / connecting
CL: none

PUBLISHED_FROM=b3704b6e1018715c88b28bb8c405d5e78ffc6673

src/mgos_pppos.c
@@ -465,7 +465,6 @@ static void mgos_pppos_dispatch_once(struct mgos_pppos_data *pd) {
         mgos_pppos_set_state(pd, PPPOS_IDLE);
         break;
       }
-      mgos_pppos_set_net_status(pd, MGOS_NET_EV_DISCONNECTED);
       mbuf_free(&pd->data);
       mbuf_init(&pd->data, 0);
       mg_strfree(&pd->ati_resp);

commit d495b9256a4ac83b40950f2c713eaae7eb1c5086

Fix regression where reset would never be called
Introduced in https://github.com/mongoose-os-libs/pppos/commit/50ef31bc08927a19b80c97ca59f2b4812721430b

Fixes https://github.com/mongoose-os-libs/pppos/issues/10

CL: pppos: Fix regression where reset would never be called

src/mgos_pppos.c
@@ -514,7 +514,7 @@ static void mgos_pppos_dispatch_once(struct mgos_pppos_data *pd) {
       }
       /* Reset modem if it's possible and we're not currently connected
        * (executing in-band user command). */
-      if (pd->net_status == MGOS_NET_EV_DISCONNECTED &&
+      if (pd->net_status == MGOS_NET_EV_CONNECTING &&
           (pd->cfg->rst_gpio >= 0 &&
            (pd->attempt == 1 || pd->cfg->rst_mode == 1))) {
         mgos_pppos_set_state(pd, PPPOS_RESET);