f4fbe1f7fb3c791aba010816b774e92b8a35ab36
ushijimamit <betashepherd@gmail.com>
2020-03-18 18:32:36 +0800

    up

--- mjs_fs/api_pppos.js
+++ mjs_fs/api_pppos.js
@@ -1,19 +1,18 @@
-
 let PPPOS = {
-    _imei: ffi('char* mgos_pppos_get_imei_raw(int)'),
-    _imsi: ffi('char* mgos_pppos_get_imsi_raw(int)'),
-    _iccid: ffi('char* mgos_pppos_get_iccid_raw(int)'),
+  _imei: ffi('const char* mgos_pppos_get_imei_raw(int)'),
+  _imsi: ffi('const char* mgos_pppos_get_imsi_raw(int)'),
+  _iccid: ffi('const char* mgos_pppos_get_iccid_raw(int)'),
 
-    imei: function() {
-        return this._imei(0);
-    },
+  imei: function() {
+    return this._imei(0);
+  },
 
-    imsi: function() {
-        return this._imsi(0);
-    },
+  imsi: function() {
+    return this._imsi(0);
+  },
 
-    iccid: function() {
-        return this._iccid(0);
-    },
+  iccid: function() {
+    return this._iccid(0);
+  },
 
 };
